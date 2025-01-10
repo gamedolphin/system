fdd75fb826f618fcb7725a06ef9bd2e069d3652e
gamedolphin <7590634+gamedolphin@users.noreply.github.com>
2025-01-10 23:48:13 +0100

    feat: cleanup commit, reset history

---
+++ home/emacs/early-init.el
@@ -0,0 +1,83 @@
+;;; package --- early init -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;; Prevents white flash and better Emacs defaults
+
+;;; Code:
+(set-language-environment "UTF-8")
+
+(setq-default
+ default-frame-alist
+ '((background-color . "#3F3F3F")
+   (bottom-divider-width . 1)            ; Thin horizontal window divider
+   (foreground-color . "#DCDCCC")        ; Default foreground color
+   (fullscreen . maximized)              ; Maximize the window by default
+   (horizontal-scroll-bars . nil)        ; No horizontal scroll-bars
+   (left-fringe . 8)                     ; Thin left fringe
+   (menu-bar-lines . 0)                  ; No menu bar
+   (right-divider-width . 1)             ; Thin vertical window divider
+   (right-fringe . 8)                    ; Thin right fringe
+   (tool-bar-lines . 0)                  ; No tool bar
+   (undecorated . t)                     ; Remove extraneous X decorations
+   (vertical-scroll-bars . nil))         ; No vertical scroll-bars
+
+ user-full-name "Sandeep Nambiar"                ; ME!
+
+ ;; memory configuration
+ ;; Higher garbage collection threshold, prevents frequent gc locks
+ gc-cons-threshold most-positive-fixnum
+ ;; Ignore warnings for (obsolete) elisp compilations
+ byte-compile-warnings '(not obsolete)
+ ;; And other log types completely
+ warning-suppress-log-types '((comp) (bytecomp))
+ ;; Large files are okay in the new millenium.
+ large-file-warning-threshold 100000000
+
+ ;; Read more based on system pipe capacity
+ read-process-output-max (max (* 10240 10240) read-process-output-max)
+
+ ;; scroll configuration
+ scroll-margin 0                                 ; Lets scroll to the end of the margin
+ scroll-conservatively 100000                    ; Never recenter the window
+ scroll-preserve-screen-position 1               ; Scrolling back and forth
+
+ ;; frame config
+ ;; Improve emacs startup time by not resizing to adjust for custom settings
+ frame-inhibit-implied-resize t
+ ;; Dont resize based on character height / width but to exact pixels
+ frame-resize-pixelwise t
+
+ ;; backups & files
+ backup-directory-alist '(("." . "~/.backups/")) ; Don't clutter
+ backup-by-copying t                             ; Don't clobber symlinks
+ create-lockfiles nil                            ; Don't have temp files
+ delete-old-versions t                           ; Cleanup automatically
+ kept-new-versions 6                             ; Update every few times
+ kept-old-versions 2                             ; And cleanup even more
+ version-control t                               ; Version them backups
+ delete-by-moving-to-trash t                     ; Dont delete, send to trash instead
+
+ ;; startup
+ inhibit-startup-screen t                        ; I have already done the tutorial. Twice
+ inhibit-startup-message t                       ; I know I am ready
+ inhibit-startup-echo-area-message t             ; Yep, still know it
+ initial-scratch-message nil                     ; I know it is the scratch buffer!
+
+ ;; tabs
+ tab-width 4                                     ; Always tab 4 spaces.
+ indent-tabs-mode nil                            ; Never use actual tabs.
+
+ ;; rendering
+ cursor-in-non-selected-windows nil              ; dont render cursors other windows
+
+ ;; packages
+ use-package-always-defer t
+
+ ;; custom
+ custom-file (concat user-emacs-directory "custom.el")
+
+ load-prefer-newer t
+
+ default-input-method nil
+ )
+  ;;; early-init.el ends here
---
+++ home/emacs/init.el
@@ -0,0 +1,311 @@
+;;; package --- Summary - My minimal Emacs init file -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;; Simple Emacs setup I carry everywhere
+
+;;; Code:
+(load custom-file 'noerror)            ;; no error on missing custom file
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+(package-initialize)
+
+(use-package emacs
+  :init
+  (global-auto-revert-mode t)          ;; revert automatically on external file changes
+  (savehist-mode)                      ;; save minibuffer history
+
+  ;; base visual
+  (menu-bar-mode -1)                   ;; no menu bar
+  (toggle-scroll-bar -1)               ;; no scroll bar
+  (tool-bar-mode -1)                   ;; no tool bar either
+  (global-hl-line-mode +1)             ;; always highlight current line
+  (blink-cursor-mode -1)               ;; stop blinking
+  (global-display-line-numbers-mode 1) ;; always show line numbers
+  (column-number-mode t)               ;; column number in the mode line
+  (size-indication-mode t)             ;; file size in the mode line
+  (pixel-scroll-precision-mode)        ;; smooth mouse scroll
+  (fset 'yes-or-no-p 'y-or-n-p)        ;; y/n is good enough
+  (electric-pair-mode)                 ;; i mean ... parens should auto create
+  (recentf-mode)                       ;; keep track of recently opened files
+
+  ;; font of the century
+  (set-frame-font "Iosevka Nerd Font 12" nil t)
+
+  :bind
+  (("C-<wheel-up>"   . pixel-scroll-precision) ; dont zoom in please, just scroll
+   ("C-<wheel-down>" . pixel-scroll-precision) ; dont zoom in either, just scroll
+   ("C-x k"          . kill-current-buffer))   ; kill the buffer, dont ask
+  )
+
+(use-package nerd-icons
+  :custom
+  ;; disable bright icon colors
+  (nerd-icons-color-icons nil))
+
+(use-package doom-modeline
+  :custom
+  (inhibit-compacting-font-caches t)    ;; speed
+  (doom-modeline-buffer-file-name-style 'relative-from-project)
+  (doom-modeline-major-mode-icon nil)   ;; distracting icons, no thank you
+  (doom-modeline-buffer-encoding nil)   ;; everything is utf-8 anyway
+  (doom-modeline-buffer-state-icon nil) ;; the filename already shows me
+  (doom-modeline-lsp nil)               ;; lsp state is too distracting, too often
+  :hook (after-init . doom-modeline-mode))
+
+(use-package doom-themes
+  :commands doom-themes-visual-bell-config
+  :custom
+  (doom-themes-enable-bold t)
+  (doom-themes-enable-italic t)
+  :init
+  (load-theme 'doom-nord t)
+  (doom-themes-visual-bell-config))
+
+(use-package diminish :demand t)         ;; declutter the modeline
+(use-package eldoc :diminish eldoc-mode) ;; docs for everything
+
+(use-package whitespace-cleanup-mode
+  :commands global-whitespace-cleanup-mode
+  :custom
+  (whitespace-cleanup-mode-only-if-initially-clean nil)
+  :hook
+  (after-init . global-whitespace-cleanup-mode))
+
+(use-package pulsar
+  :commands pulsar-global-mode pulsar-recenter-top pulsar-reveal-entry
+  :init
+  (defface pulsar-nord
+    '((default :extend t)
+      (((class color) (min-colors 88) (background light))
+       :background "#2e3440")
+      (((class color) (min-colors 88) (background dark))
+       :background "#81a1c1")
+      (t :inverse-video t))
+    "Alternative nord face for `pulsar-face'."
+    :group 'pulsar-faces)
+  :custom
+  (pulsar-face 'pulsar-nord)
+  :hook
+  (after-init . pulsar-global-mode))
+
+(use-package which-key
+  :commands which-key-mode
+  :diminish which-key-mode
+  :hook
+  (after-init . which-key-mode))
+
+(use-package expreg
+  :bind ("M-m" . expreg-expand))
+
+(use-package vundo) ;; undo tree
+
+;; better structured editing
+(use-package puni
+  :commands puni-global-mode
+  :hook
+  (after-init . puni-global-mode))
+
+(use-package avy
+  :bind
+  ("M-i" . avy-goto-char-2)
+  :custom
+  (avy-background t))
+
+(use-package consult
+  :bind
+  ("C-x b"   . consult-buffer)     ;; orig. switch-to-buffer
+  ("M-y"     . consult-yank-pop)   ;; orig. yank-pop
+  ("M-g M-g" . consult-goto-line)  ;; orig. goto-line
+  :custom
+  (consult-narrow-key "<"))
+
+(use-package vertico
+  :commands vertico-mode
+  :custom
+  (read-file-name-completion-ignore-case t)
+  (read-buffer-completion-ignore-case t)
+  (completion-ignore-case t)
+  (enable-recursive-minibuffers t)
+  :init
+  (vertico-mode)
+  :config
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  :hook
+  (minibuffer-setup-hook . cursor-intangible-mode))
+
+(use-package marginalia
+  :commands marginalia-mode
+  :hook (after-init . marginalia-mode))
+
+(use-package crux
+  :bind
+  ("C-c M-e" . crux-find-user-init-file)
+  ("C-c C-w" . crux-transpose-windows)
+  ("C-a"     . crux-move-beginning-of-line))
+
+(use-package magit
+  :bind (("C-M-g" . magit-status)))
+
+(use-package nerd-icons-corfu
+  :commands nerd-icons-corfu-formatter
+  :defines corfu-margin-formatters)
+
+(use-package corfu
+  :commands global-corfu-mode
+  :custom
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-delay  1)
+  (corfu-auto-prefix 3)
+  (corfu-separator ?_)
+  :hook
+  (after-init . global-corfu-mode)
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+(use-package cape)
+
+(use-package orderless
+  :custom
+  (completion-styles '(orderless partial-completion basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides nil))
+
+(use-package yasnippet
+  :commands yas-global-mode
+  :diminish yas-minor-mode
+  :hook
+  (after-init . yas-global-mode))
+
+(use-package yasnippet-snippets :after yasnippet)
+
+(use-package projectile
+  :commands projectile-mode
+  :diminish projectile-mode
+  :custom
+  (projectile-globally-ignored-directories (append '("node_modules")))
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :config
+  (projectile-mode +1))
+
+(use-package exec-path-from-shell
+  :commands exec-path-from-shell-initialize
+  :custom
+  (exec-path-from-shell-arguments nil)
+  :hook
+  (after-init . exec-path-from-shell-initialize))
+
+(use-package flycheck
+  :commands global-flycheck-mode
+  :diminish
+  :hook
+  (after-init . global-flycheck-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred lsp-format-buffer
+                 lsp-organize-imports
+                 orderless-dispatch-flex-first
+                 cape-capf-buster lsp-completion-at-point)
+  :defines lsp-file-watch-ignored-directories
+  :diminish lsp-lens-mode
+  :bind-keymap
+  ("C-c l" . lsp-command-map)
+  :custom
+    (lsp-lens-enable nil)
+    (lsp-idle-delay 0.500)
+    (lsp-modeline-code-actions-enable t)
+    (lsp-modeline-diagnostics-enable t)
+    (lsp-csharp-omnisharp-roslyn-binary-path "OmniSharp")
+    (lsp-completion-provider :none) ;; we use Corfu!
+    (lsp-eldoc-render-all t)
+    :init
+    (defun orderless-dispatch-flex-first (_pattern index _total)
+      (and (eq index 0) 'orderless-flex))
+
+    ;; Configure the first word as flex filtered.
+    (add-hook 'orderless-style-dispatchers #'orderless-dispatch-flex-first nil 'local)
+
+    (defun lsp-mode-setup-completion ()
+      (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+            '(orderless)))
+
+    ;; Optionally configure the cape-capf-buster.
+    (setq-local completion-at-point-functions
+                (list (cape-capf-buster #'lsp-completion-at-point)))
+    :config
+    (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\Temp\\'")
+    (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\Logs\\'")
+    (defun lsp-cleanup ()
+      (lsp-format-buffer)
+      (lsp-organize-imports)
+      (whitespace-cleanup))
+    :hook
+    (lsp-completion-mode . lsp-mode-setup-completion)
+    (lsp-mode . lsp-enable-which-key-integration)
+    (before-save . lsp-cleanup)
+    (prog-mode . lsp-deferred))
+
+  (use-package lsp-ui :commands lsp-ui-mode
+    :custom
+    (lsp-ui-doc-enable t)
+    (lsp-ui-sideline-diagnostic-max-lines 4)
+    (lsp-ui-doc-show-with-mouse nil)
+    (lsp-ui-doc-position 'bottom)
+    (lsp-ui-doc-show-with-cursor t)
+    (lsp-eldoc-enable-hover nil)
+    )
+
+  (use-package nixpkgs-fmt
+    :custom
+    (nixpkgs-fmt-command "nixfmt"))
+
+  (use-package rust-mode
+    :custom
+    (rust-mode-treesitter-derive t)
+    (lsp-rust-analyzer-cargo-watch-command "clippy")
+    (lsp-rust-analyzer-exclude-dirs ["Temp/**"]))
+
+  (use-package typescript-ts-mode
+    :custom
+    (lsp-javascript-preferences-import-module-specifier :relative)
+    (typescript-indent-level 2)
+    (typescript-ts-mode-indent-offset 2))
+
+  (use-package eat)
+  (use-package hcl-mode)
+  (use-package jinja2-mode)
+  (use-package f :demand t)
+
+  (use-package envrc
+    :commands envrc-global-mode
+    :hook
+    (after-init . envrc-global-mode))
+
+  (use-package nix-mode
+    :hook (nix-mode . lsp-deferred))
+
+  (use-package shell-pop
+    :custom
+    (shell-pop-universal-key "M-o"))
+
+  (use-package copilot
+    :defines copilot-completion-map
+    :bind
+    (:map copilot-completion-map
+          ("<tab>" . copilot-accept-completion)
+          ("M-n" . copilot-next-completion)
+          ("M-p" . copilot-previous-completion)
+          ("C-g" . copilot-clear-overlay)))
+
+  (use-package gptel
+    :commands gptel-make-anthropic f-read-text
+    :config
+    (gptel-make-anthropic "Claude"
+      :stream t :key (f-read-text "/run/secrets/claude_key")))
+
+  (provide 'init)
+
+  ;;; init.el ends here
