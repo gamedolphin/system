759bf57cec2f2770e418d58aaac16702937a8360
gamedolphin <7590634+gamedolphin@users.noreply.github.com>
2025-04-15 12:33:00 +0000

    Tangle files from the README.

--- machines/smallbox/configuration.nix
+++ machines/smallbox/configuration.nix
@@ -18,7 +18,7 @@
     mode = "644";
     path = "/home/${user.username}/.ssh/id_ed25519.pub";
   };
-
+  
   sops.secrets."ssh/wavefunk/private" = { # ssh private key
     owner = "${user.username}";
     mode = "600";
